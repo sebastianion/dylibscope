60ef615febb841bf6499ca4ae9fe86a4530a52a7
sebastianion <sebastianion@outlook.com>
2026-05-23 19:42:22 +0300

    Remove deprecated plotting files

--- src/dylibscope/high_level_analysis/plot_hla_graph.py
+++
@@ -1,6 +0,0 @@
-from dylibscope.analysis_graph.config.high_level_analysis_config import HLA, HLA_OUTPUT_FILE_NAME
-from dylibscope.analysis_graph.plot_graph import Graph
-from dylibscope.config.datasets import HLA_INPUT
-
-if __name__ == "__main__":
-    Graph(HLA_INPUT, HLA).render(HLA_OUTPUT_FILE_NAME)
--- src/dylibscope/low_level_analysis/plot_lla_graph.py
+++
@@ -1,6 +0,0 @@
-from dylibscope.analysis_graph.config.low_level_analysis_config import LLA, LLA_OUTPUT_FILE_NAME
-from dylibscope.analysis_graph.plot_graph import Graph
-from dylibscope.config.datasets import LLA_INPUT
-
-if __name__ == "__main__":
-    Graph(LLA_INPUT, LLA).render(LLA_OUTPUT_FILE_NAME)
